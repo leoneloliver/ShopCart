12725a51b8528c9ba8757ec75092edda71735ded
Leonel Oliveira <leoneloliver@gmail.com>
2016-12-30 16:24:55 -0500

    Update cart.js

--- cart.js
+++ cart.js
@@ -1,6 +1,6 @@
 var cart_sum =0;
 $( ".unit" ).each(function( index ) {
-		cart_sum = cart_sum+$( this ).text()*1;
+	cart_sum = cart_sum+$( this ).text()*1;
 });
 
 $('.total-product').html(cart_sum+' Product(s)');
